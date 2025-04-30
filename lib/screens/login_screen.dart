5af2a6a1ae73227270d747ebad16170b90f70630
Kboy <villahermosafrancisco6@gmail.com>
2025-04-30 23:22:32 +0800

    update the login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -151,7 +151,7 @@ class _LoginScreenState extends State<LoginScreen> {
                       ),
                       const SizedBox(height: 32),
                       Text(
-                        'Welcome Back',
+                        'ServeBisyo',
                         style: TextStyle(
                           fontSize: 28,
                           fontWeight: FontWeight.bold,
